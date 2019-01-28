d444f18ff8e7aefe79e26cf3c13992ca2161288a
tsafacjo <jordanetsafack@yahoo.fr>
2019-01-28 14:52:45 +0100

    updata test

--- src/testVerificationValidation/testPointGenrerated.java
+++ src/testVerificationValidation/testPointGenrerated.java
@@ -1,18 +1,19 @@
-package test;
+package testVerificationValidation;
 
 import java.util.Map;
 
+import enstabretagne.travaux_diriges.TD_corrige.BasicMovement.SimEntity.MouvementSequenceur.GeneratePoints;
 import javafx.geometry.Point3D;
-import utils.GeneratePoints;
 
 public class testPointGenrerated {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GeneratePoints g = new GeneratePoints(new Point3D(0,0,0) , new Point3D(-100,-100,0), 4);
+		GeneratePoints g = new GeneratePoints(new Point3D(0,0,0) , new Point3D(800,0,0), 4);
 		Map<String, Point3D> Point = g.getPoints();
 		//Map<Integer, Map<String, Point3D>> Point = g.getPoints();
 		System.out.println("size : "+Point.size()+"\nPoints"+Point);
+		
 		/*
 		for (Integer key : Point.keySet()) {
 			Map<String, Point3D> p = Point.get(key);
