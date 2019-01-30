d5dbb01789fb851367afd24c087067b20b2888f9
cedricgraf <aurel.cedric12@gmail.com>
2019-01-30 17:12:34 +0100

    test fonctionnel

--- src/testVerificationValidation/ValidationMouvementDrone.java
+++ src/testVerificationValidation/ValidationMouvementDrone.java
@@ -1,10 +1,16 @@
 package testVerificationValidation;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import com.codoid.products.exception.FilloException;
+import com.codoid.products.fillo.Recordset;
 
 public class ValidationMouvementDrone extends AbstractTest {
+	ArrayList<String> listeMouvements = new ArrayList<String>();
+	ArrayList<String> liste = new ArrayList<String>();
+	int cpt=0;
 	
 	@Test
 	public void test() throws FilloException {
@@ -19,7 +25,33 @@ public class ValidationMouvementDrone extends AbstractTest {
 
 	@Override
 	public void query() throws FilloException {
+		String strQuery="Select * from Logs where Message like 'Start%'";
+		Recordset recordset=connection.executeQuery(strQuery);
+		while(recordset.next()){
+			listeMouvements.add(recordset.getField("Message"));
+		}
+		recordset.close();
+
 		
+	}
+	
+	public boolean testMouvement() {
+		
+		for (int i=0; i<listeMouvements.size(); i++){
+			liste.add(listeMouvements.get(i));
+			System.out.println("----------" + listeMouvements.get(i));
+			liste.spliterator();
+
+		}
+		for(int j=0; j<liste.size();j++){
+			if((liste.get(j).contains("Lineaire")) && (liste.get(j+1).contains("circulaire"))){
+				cpt++;
+			}
+		}
+		if(cpt++ >=2 )
+			return true;
+	
+		return false;
 		
 	}
 
