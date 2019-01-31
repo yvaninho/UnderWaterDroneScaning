db6c4f314e46cfd67a174fa317b1b10c15111a74
tsafacjo <jordanetsafack@yahoo.fr>
2019-01-31 08:08:04 +0100

    constantes

---
+++ src/enstabretagne/travaux_diriges/TD_corrige/BasicMovement/Messages/Constantes.java
@@ -0,0 +1,8 @@
+package enstabretagne.travaux_diriges.TD_corrige.BasicMovement.Messages;
+
+import enstabretagne.base.time.LogicalDateTime;
+
+public class Constantes {
+
+public static LogicalDateTime tempsDepart = new LogicalDateTime("05/12/2017 06:00");
+}
